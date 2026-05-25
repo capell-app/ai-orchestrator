71840d15d9011e95e35c62856bfe5e133a7388db
Ben <howdu@users.noreply.github.com>
2026-05-25 01:40:42 +0100

    Tweaks

--- src/Providers/AIOrchestratorServiceProvider.php
+++ src/Providers/AIOrchestratorServiceProvider.php
@@ -8,6 +8,7 @@ use Capell\AIOrchestrator\Integrations\LayoutBuilder\LayoutBuilderAIOrchestrator
 use Capell\AIOrchestrator\Support\AIOrchestratorModuleRegistry;
 use Capell\Core\Facades\CapellCore;
 use Capell\Core\Support\Packages\AbstractPackageServiceProvider;
+use Override;
 use Spatie\LaravelPackageTools\Package;
 
 class AIOrchestratorServiceProvider extends AbstractPackageServiceProvider
@@ -35,6 +36,7 @@ class AIOrchestratorServiceProvider extends AbstractPackageServiceProvider
         });
     }
 
+    #[Override]
     protected function isPackageInstalled(): bool
     {
         return CapellCore::isPackageInstalled(static::$packageName);
